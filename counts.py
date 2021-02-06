e2910d696d0cb081c6d5bb3c37b5833fe997b9f8
care1e55 <sfilms89@gmail.com>
2021-02-07 01:02:04 +0300

    counts

---
+++ counts.py
@@ -0,0 +1,37 @@
+from typing import Dict, Set, Any
+# Number of characters (including whitespace)
+# Number of words (separated by whitespace)
+# Number of lines
+# Number of unique words (case sensitive, so “NO” is different from “no”)
+
+file_path = "wcfile.txt"
+
+def wc(file_path: str) -> Dict[str, int]:
+    """
+    Count:
+        Number of characters (including whitespace)
+        Number of words (separated by whitespace)
+        Number of lines
+        Number of unique words (case sensitive, so “NO” is different from “no”)
+    """
+    unique_words = set()
+    result = {
+        "num_chars": 0,
+        "num_words": 0,
+        "num_lines": 0,
+        "num_unique_words": 0
+    }
+    with open(file_path) as file:
+        for line in file:
+            result["num_chars"] += len(line)
+            result["num_words"] += len(line.split())
+            result["num_lines"] += 1
+            unique_words.update(line.split())
+    result["num_unique_words"] = len(unique_words)
+    return result
+
+def print_result(result: Dict[str, int]):
+    for items in result.items():
+        print(f'{items[0]}: {items[1]}')        
+
+print_result(wc(file_path))
