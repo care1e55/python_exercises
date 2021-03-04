8271a147d1f2e8b9c73366837bdff2f6362a19c7
care1e55 <sfilms89@gmail.com>
2021-03-04 22:43:27 +0300

    real rocket science

--- fredonia.py
+++ fredonia.py
@@ -8,9 +8,16 @@ province_tax_rates = {
     'zeppo': 0.4
 }
 
+class HourTooLowError(Exception): pass
+class HourTooHighError(Exception): pass
+
 def time_percentage(hour):
     return hour / 24.0
 
 
 def calculate_tax(amount: int, province: str, purchase_time: int) -> int:
+    if purchase_time < 0:
+        raise HourTooLowError(f'Hour of {purchase_time} is < 0')
+    if purchase_time >= 24:
+        raise HourTooHighError(f'Hour of {purchase_time} is >= 24')
     return amount * province_tax_rates[province.lower()] * time_percentage(purchase_time)
--- use_fredonia.py
+++ use_fredonia.py
@@ -2,3 +2,5 @@ from fredonia import calculate_tax
 
 if __name__=="__main__":
     print(f"The taxes are {calculate_tax(500, 'Harpo', 12)}")
+    print(f"The taxes are {calculate_tax(500, 'Harpo', -1)}")
+    print(f"The taxes are {calculate_tax(500, 'Harpo', 111)}")
