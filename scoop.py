3b1e4c7747e4f4d60e0a35bed880465c40f2d634
care1e55 <sfilms89@gmail.com>
2021-03-20 00:11:28 +0300

    scoop

--- scoop.py
+++ scoop.py
@@ -7,6 +7,18 @@ class Scoop():
     def __repr__(this) -> str:
         return this.flavor
 
+
+class Bowl():
+    def __init__(this):
+        this.bowl = []
+
+    def add_scoops(this, *args):
+        this.bowl += [*args]
+
+    def __repr__(this):
+        return ", ".join(scoop.__repr__() for scoop in this.bowl)
+
+
 def create_scoops() -> List[str]:
     return [
         Scoop('chocolate'),
@@ -14,5 +26,11 @@ def create_scoops() -> List[str]:
         Scoop('persimmon'),
     ]
 
+
 if __name__ == "__main__":
-    print(create_scoops())
+    # print(create_scoops())
+    s1, s2, s3 = Scoop('chocolate'), Scoop('vanilla'), Scoop('persimmon')
+    b = Bowl()
+    b.add_scoops(s1)
+    b.add_scoops(s3, s2)
+    print(b)
