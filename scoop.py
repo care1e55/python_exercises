82b8cb762885676a110fb6703723d75ec7ef1fcd
care1e55 <sfilms89@gmail.com>
2021-03-21 23:04:05 +0300

    scoop

--- scoop.py
+++ scoop.py
@@ -9,12 +9,19 @@ class Scoop():
 @dataclass
 class Bowl():
     bowl: List[Scoop] = field(default_factory=list)
+    max_scoops = 3
 
-    def add_scoops(this, *args):
-        this.bowl += [*args]
+    def add_scoops(this, *new_scoops):
+        for scoop in new_scoops:
+            if len(this.bowl) < this.max_scoops:
+                this.bowl.append(scoop)
 
     def __repr__(this) -> str:
         return ", ".join(scoop.flavor for scoop in this.bowl)
+
+
+class BigBowl(Bowl):
+    max_scoops = 5
 
 
 def create_scoops() -> List[str]:
@@ -26,9 +33,12 @@ def create_scoops() -> List[str]:
 
 
 if __name__ == "__main__":
-    print(create_scoops())
     s1, s2, s3 = Scoop('chocolate'), Scoop('vanilla'), Scoop('persimmon')
     b = Bowl()
-    b.add_scoops(s1)
-    b.add_scoops(s3, s2)
-    print(b)
+    bb = BigBowl()
+    bb.add_scoops(s1)
+    print(bb.bowl)
+    bb.add_scoops(s3, s2)
+    bb.add_scoops(s3, s2)
+    bb.add_scoops(create_scoops())
+    print(bb)
